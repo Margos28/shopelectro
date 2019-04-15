c531f3162c996a0b46608f7652519c01075a0432
bnikodem <bartosz.nikodem@intenso.pl>
2019-04-15 21:37:41 +0200

    initial commit

---
+++ src/sklep/Phone.java
@@ -0,0 +1,28 @@
+package sklep;
+
+public class Phone {
+	private int id;
+	private String brand;
+	private String model;
+	private int cameraResolution;
+
+	Phone(int id, String brand, String model, int cameraResolution) {
+		this.id = id;
+		this.brand = brand;
+		this.model = model;
+		this.cameraResolution = cameraResolution;
+	}
+	int getId() {
+		return this.id;
+	}
+	void showDetails() {
+		System.out.println(this.toString());
+		//System.out.println("marka: " + this.brand + " model: " + this.model + " rozdzielczość aparatu: " + this.cameraResolution);
+	}
+
+	@Override
+	public String toString() {
+		return "marka: " + this.brand + " model: " + this.model + " rozdzielczość aparatu: " + this.cameraResolution;
+	}
+
+}
