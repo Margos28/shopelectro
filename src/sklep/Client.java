72c22aa06ea3158ef2c7701802b958aa40d45c00
Margos28 <49654396+Margos28@users.noreply.github.com>
2019-05-01 15:26:23 +0200

    Update Client.java

--- src/sklep/Client.java
+++ src/sklep/Client.java
@@ -9,15 +9,17 @@ public class Client {
 	private String lastName;
 	private List<Phone> boughtphones;
 	private List<Tv> boughtTvs;
-
+	private int totalamount;
+	
 	Client(int id, String name, String lastName) {
 		this.id = id;
 		this.name = name;
 		this.lastName = lastName;
 		this.boughtphones = new ArrayList<>();
 		this.boughtTvs = new ArrayList<>();
+		this.totalamount = 0;
 	}
-
+	
 	void showDetails() {
 		System.out.println(
 				"Imię: " + this.name + " nazwisko: " + this.lastName + "\n kupione telefony: \n" + this.boughtphones + "\n kupione tv: \n"+ this.boughtTvs);
@@ -36,8 +38,32 @@ public class Client {
 		}else
 			System.out.println("limit wyczerpany :( ");
 	}
-	void removePhone(int phone) {
+	void removePhone(Phone phone) {
 			boughtphones.remove(phone);
 		}
+	void removeTv(Tv tv) {
+		boughtTvs.remove(tv);
 	}
-}
+	void getTotalAmount()
+	{
+		totalamount = 0;
+		for(int i = 0; i < boughtTvs.size(); i++ ) {
+			 boughtTvs.get(i).getPrice();	
+		
+		for(int j = 0; j < boughtphones.size(); j++)
+		{
+			boughtphones.get(j).getPrice();
+			totalamount =  boughtTvs.get(i).getPrice()+boughtphones.get(j).getPrice() ;	
+		}
+		}
+		
+		System.out.println();
+		System.out.println(this.totalamount);
+		
+		
+	}
+	
+	
+	}
+
+
